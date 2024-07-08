982681ff5df14f2b6b563ee4885dd1995c643cea
Jakub Gramburg <jacubgr@gmail.com>
2024-07-08 21:18:06 +0200

    Finish UT for ClientsApi endpoint

--- Tuudio.UnitTests/ApplicationTests/Endpoints/ClientsApiTests.cs
+++ Tuudio.UnitTests/ApplicationTests/Endpoints/ClientsApiTests.cs
@@ -3,6 +3,7 @@ using FluentValidation.Results;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using Tuudio.Domain.Entities.People;
+using Tuudio.Domain.Exceptions;
 using Tuudio.DTOs.People;
 using Tuudio.DTOs.People.Detailed;
 using Tuudio.Endpoints;
@@ -13,8 +14,8 @@ namespace Tuudio.UnitTests.ApplicationTests.Endpoints;
 [TestFixture]
 public class ClientsApiTests
 {
-    private readonly Mock<IClientRepository> repositoryMock = new();
-    private readonly Mock<IUnitOfWork> uowMock = new();
+    private Mock<IClientRepository> repositoryMock;
+    private Mock<IUnitOfWork> uowMock;
 
     private readonly List<Client> twoClientList =
     [
@@ -27,7 +28,8 @@ public class ClientsApiTests
     [SetUp]
     public void SetUp()
     {
-
+        repositoryMock = new();
+        uowMock = new();
     }
 
     [Test]
@@ -215,5 +217,181 @@ public class ClientsApiTests
         castedResult.StatusCode.ShouldBe(400);
         castedResult.Value.ShouldNotBeNull();
         castedResult.Value.ShouldContain(validationFailure);
+    }
+
+    [Test]
+    public async Task UpdateClientAsync_ValidDtoProvided_ShouldReturn201AndUpdatedDetailedDto()
+    {
+        // Arrange
+        var clientDto = new ClientDto()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+        };
+
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        var validatorMock = new Mock<IValidator<ClientDto>>();
+        validatorMock
+            .Setup(m => m.ValidateAsync(It.IsAny<ClientDto>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+
+        // Act
+        var result = await ClientsApi.UpdateClientAsync(uowMock.Object, firstGuid, clientDto, validatorMock.Object);
+
+        // Assert
+        result.ShouldBeOfType(typeof(Ok<ClientDetailedDto>));
+
+        var castedResult = (Ok<ClientDetailedDto>)result;
+
+        castedResult.StatusCode.ShouldBe(200);
+
+        castedResult.Value.ShouldNotBeNull();
+        castedResult.Value.Id.ShouldNotBe(Guid.Empty);
+        castedResult.Value.FirstName = clientDto.FirstName;
+        castedResult.Value.LastName = clientDto.LastName;
+        castedResult.Value.Email = null;
+        castedResult.Value.PhoneNumber = null;
+
+        var id = castedResult.Value.Id;
+    }
+
+    [Test]
+    public async Task UpdateClientAsync_NullDtoProvided_ShouldReturn400AndErrorMessage()
+    {
+        // Arrange
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        var validatorMock = new Mock<IValidator<ClientDto>>();
+        validatorMock
+            .Setup(m => m.ValidateAsync(It.IsAny<ClientDto>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+
+        // Act
+        var result = await ClientsApi.UpdateClientAsync(uowMock.Object, firstGuid, null!, validatorMock.Object);
+
+        // Assert
+        result.ShouldBeOfType(typeof(BadRequest<string>));
+
+        var castedResult = (BadRequest<string>)result;
+
+        castedResult.StatusCode.ShouldBe(400);
+        castedResult.Value.ShouldNotBeNull();
+        castedResult.Value.ShouldBe("Client data is required.");
+    }
+
+    [Test]
+    public async Task UpdateClientAsync_EmptyIdProvided_ShouldReturn400AndErrorMessage()
+    {
+        // Arrange
+        var clientDto = new ClientDto()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+        };
+
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        var validatorMock = new Mock<IValidator<ClientDto>>();
+        validatorMock
+            .Setup(m => m.ValidateAsync(It.IsAny<ClientDto>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+
+        // Act
+        var result = await ClientsApi.UpdateClientAsync(uowMock.Object, Guid.Empty, clientDto, validatorMock.Object);
+
+        // Assert
+        result.ShouldBeOfType(typeof(BadRequest<string>));
+
+        var castedResult = (BadRequest<string>)result;
+
+        castedResult.StatusCode.ShouldBe(400);
+        castedResult.Value.ShouldNotBeNull();
+        castedResult.Value.ShouldBe("Client id is required.");
+    }
+
+    [Test]
+    public async Task UpdateClientAsync_ValidatorReturnsInvalidDtoData_ShouldReturn400AndValidationErrors()
+    {
+        // Arrange
+        ValidationFailure validationFailure = new("FirstName", "Invalid Value");
+        ValidationResult validationResult = new(new List<ValidationFailure> { validationFailure });
+
+        var clientDto = new ClientDto()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+        };
+
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        var validatorMock = new Mock<IValidator<ClientDto>>();
+        validatorMock
+            .Setup(m => m.ValidateAsync(It.IsAny<ClientDto>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(validationResult);
+
+        // Act
+        var result = await ClientsApi.UpdateClientAsync(uowMock.Object, firstGuid, clientDto, validatorMock.Object);
+
+        // Assert
+        result.ShouldBeOfType(typeof(BadRequest<List<ValidationFailure>>));
+
+        var castedResult = (BadRequest<List<ValidationFailure>>)result;
+
+        castedResult.StatusCode.ShouldBe(400);
+        castedResult.Value.ShouldNotBeNull();
+        castedResult.Value.ShouldContain(validationFailure);
+    }
+
+    [Test]
+    public async Task DeleteClientAsync_ValidIdProvided_ShouldReturn204()
+    {
+        // Arrange
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        // Act
+        var result = await ClientsApi.DeleteClientAsync(uowMock.Object, It.IsAny<Guid>());
+
+        // Assert
+        result.ShouldBeOfType(typeof(NoContent));
+
+        var castedResult = (NoContent)result;
+
+        castedResult.StatusCode.ShouldBe(204);
+    }
+
+    [Test]
+    public async Task DeleteClientAsync_InvalidIdProvided_ShouldReturn404()
+    {
+        // Arrange
+        repositoryMock
+            .Setup(m => m.DeleteAsync(It.IsAny<Guid>()))
+            .ThrowsAsync(new ClientNotFoundException(It.IsAny<Guid>()));
+        
+        uowMock
+            .Setup(m => m.ClientRepository)
+            .Returns(repositoryMock.Object);
+
+        // Act
+        var result = await ClientsApi.DeleteClientAsync(uowMock.Object, It.IsAny<Guid>());
+
+        // Assert
+        result.ShouldBeOfType(typeof(NotFound<string>));
+
+        var castedResult = (NotFound<string>)result;
+
+        castedResult.StatusCode.ShouldBe(404);
+        castedResult.Value.ShouldNotBeNull();
+        castedResult.Value.ShouldBe("Entity of type 'Tuudio.Domain.Entities.People.Client' with ID = '00000000-0000-0000-0000-000000000000' not found");
     }
 }
