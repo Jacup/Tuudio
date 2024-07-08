e6caf69b823fb4505b5d641253e6799ad2e82b92
Jakub Gramburg <jacubgr@gmail.com>
2024-07-08 21:50:21 +0200

    Create entity, repository and migration

---
+++ Tuudio.Infrastructure/Migrations/20240708194547_CreateActivtyEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Tuudio.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateActivtyEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Activities",
+                columns: new[] { "Id", "CreatedDate", "Description", "Name", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { new Guid("00000000-0000-0000-0001-000000000001"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Yoga group class", "Yoga", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { new Guid("00000000-0000-0000-0001-000000000002"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Individual pool session", "Pool", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { new Guid("00000000-0000-0000-0001-000000000003"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "EMS training", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activities");
+        }
+    }
+}
---
+++ Tuudio.Infrastructure/Services/Repositories/ActivityRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tuudio.Domain.Entities.Activities;
+using Tuudio.Infrastructure.Data;
+using Tuudio.Infrastructure.Services.Interfaces;
+
+namespace Tuudio.Infrastructure.Services.Repositories;
+
+public class ActivityRepository(TuudioDbContext context) : GenericRepository<Activity>(context), IActivityRepository
+{
+}
