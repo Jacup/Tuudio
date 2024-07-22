79cb382547ac29aabcddf1d2f4ac5a0231cde743
Jakub Gramburg <jacubgr@gmail.com>
2024-07-22 22:24:57 +0200

    Add domain data

--- Tuudio.Domain/Entities/Activities/Activity.cs
+++ Tuudio.Domain/Entities/Activities/Activity.cs
@@ -1,8 +1,12 @@
-﻿namespace Tuudio.Domain.Entities.Activities;
+﻿using Tuudio.Domain.Entities.PassTemplates;
+
+namespace Tuudio.Domain.Entities.Activities;
 
 public class Activity : DbObject
 {
     public required string Name { get; set; }
 
     public string? Description { get; set; }
+
+    //public ICollection<PassTemplate> PassTemplates { get; set; } = [];
 }
---
+++ Tuudio.Domain/Enums/Period.cs
@@ -0,0 +1,12 @@
+﻿namespace Tuudio.Domain.Enums;
+
+public enum Period
+{
+    Hour,
+    Day,
+    Week,
+    Month,
+    Quarter,
+    Year,
+    Lifetime
+}
