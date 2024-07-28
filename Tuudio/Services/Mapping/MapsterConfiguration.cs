c62195e52bba24ac3f35fd2bc070d748abae4892
Jakub Gramburg <jacubgr@gmail.com>
2024-07-28 16:17:33 +0200

    mapster and pt

--- Tuudio/Services/Mapping/MapsterConfiguration.cs
+++ Tuudio/Services/Mapping/MapsterConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Mapster;
 using Tuudio.Domain.Entities.Activities;
+using Tuudio.Domain.Entities.PassTemplates;
 using Tuudio.DTOs;
 
 namespace Tuudio.Services.Mapping;
@@ -8,9 +9,28 @@ public static class MapsterConfiguration
 {
     public static void Configure()
     {
-        TypeAdapterConfig<ActivityDto, Activity>.NewConfig().Ignore(dest => dest.PassTemplates);
+        // Activities
+        TypeAdapterConfig<ActivityDto, Activity>.NewConfig()
+            .Ignore(dest => dest.PassTemplates);
 
-        TypeAdapterConfig<Activity, ActivityDetailedDto>.NewConfig().Map(dest => dest.PassTemplates, src => src.PassTemplates.Select(pt => pt.Id).ToList());
+        TypeAdapterConfig<Activity, ActivityDetailedDto>.NewConfig()
+            .Map(dest => dest.PassTemplates, src => src.PassTemplates.Select(pt => pt.Id).ToList());
+
+        // PassTemplates
+        TypeAdapterConfig<PassTemplateDto, PassTemplate>.NewConfig()
+            .Ignore(dest => dest.Activities)
+            .Map(dest => dest.Price.Amount, src => src.Price_Amount)
+            .Map(dest => dest.Price.Period, src => src.Price_Period)
+            .Map(dest => dest.Duration.Amount, src => src.Duration_Amount)
+            .Map(dest => dest.Duration.Period, src => src.Duration_Period);
+
+
+        TypeAdapterConfig<PassTemplate, PassTemplateDetailedDto>.NewConfig()
+            .Map(dest => dest.Price_Amount, src => src.Price.Amount)
+            .Map(dest => dest.Price_Period, src => src.Price.Period)
+            .Map(dest => dest.Duration_Amount, src => src.Duration.Amount)
+            .Map(dest => dest.Duration_Period, src => src.Duration.Period)
+            .Map(dest => dest.Activities, src => src.Activities.Select(a => a.Id).ToList());
     }
 }
 
