c1fce07ec8cf53704c9bd9828cecdd8033c8f73d
Jakub Gramburg <jacubgr@gmail.com>
2024-08-08 22:46:43 +0200

    Add entry model

---
+++ Tuudio.Domain/Entities/Entries/Entry.cs
@@ -0,0 +1,19 @@
+﻿using Tuudio.Domain.Entities.Passes;
+using Tuudio.Domain.Entities.People;
+
+namespace Tuudio.Domain.Entities.Entries;
+
+public class Entry : DbObject
+{
+    public required DateTime EntryDate { get; set; }
+
+    public string? Note { get; set; }
+
+    public Guid ClientId { get; set; }
+    
+    public Client Client { get; set; }
+
+    public Guid? PassId { get; set; }
+
+    public Pass? Pass { get; set; }
+}
